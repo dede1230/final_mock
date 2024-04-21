ed0c7a91da15a9dc95d1a2a4e19152faaa645b2a
Hanh Bui <dede@MacBook-Air-cua-Hanh.local>
2024-04-21 20:31:24 +0700

    First commit

---
+++ hurst/src/main/java/com/fpt/hurst/HurstApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.hurst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurstApplication.class, args);
+	}
+
+}
---
+++ hurst/src/test/java/com/fpt/hurst/HurstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.hurst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
