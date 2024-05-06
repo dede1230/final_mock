9d65a70cb294e8bcb7edcc42f52411b64c805b68
husone <sonhvnde160056@fpt.edu.vn>
2024-05-06 11:51:23 +0700

    feat: Add IndexController for handling index page request

--- hurst/src/main/java/com/fpt/hurst/HurstApplication.java
+++ hurst/src/main/java/com/fpt/hurst/HurstApplication.java
@@ -2,12 +2,12 @@ package com.fpt.hurst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HurstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HurstApplication.class, args);
 	}
-
 }
