943de1f382f6886810ab184299051cee1e482a23
husone <sonhvnde160056@fpt.edu.vn>
2024-05-09 17:35:25 +0700

    chore: Update pom.xml and add log4j2 dependency

--- hurst/src/main/java/com/fpt/hurst/HurstApplication.java
+++ hurst/src/main/java/com/fpt/hurst/HurstApplication.java
@@ -1,10 +1,11 @@
 package com.fpt.hurst;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class HurstApplication {
 
 	public static void main(String[] args) {
--- hurst/src/main/java/com/fpt/hurst/mapper/CakeMapper.java
+++ hurst/src/main/java/com/fpt/hurst/mapper/CakeMapper.java
@@ -8,11 +8,9 @@ import org.apache.ibatis.annotations.Select;
 import com.fpt.hurst.dto.Cake;
 
 @Mapper
-
 public interface CakeMapper {
 
 	@Select("select id, name, price, description, information, image, likes, created_at, updated_at\n" + "from CAKE")
-
 	public List<Cake> getCakelist();
 
 }
