3c794a50f2c35e418a42f5af7e080de41db4366b
husone <sonhvnde160056@fpt.edu.vn>
2024-05-12 18:51:39 +0700

    feat: Update CakeMapper to include createdAt and updatedAt properties
    The CakeMapper interface has been updated to include the `createdAt` and `updatedAt` properties in the `@Select` query. This change allows the `getCakelist()` method to retrieve the creation and update timestamps for each cake.

--- hurst/src/main/java/com/fpt/hurst/mapper/CakeMapper.java
+++ hurst/src/main/java/com/fpt/hurst/mapper/CakeMapper.java
@@ -3,6 +3,7 @@ package com.fpt.hurst.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
 
 import com.fpt.hurst.dto.Cake;
@@ -11,6 +12,8 @@ import com.fpt.hurst.dto.Cake;
 public interface CakeMapper {
 
 	@Select("select id, name, price, description, information, image, likes, created_at, updated_at\n" + "from CAKE")
+	@Result(property = "createAt", column = "created_at")
+	@Result(property = "updateAt", column = "updated_at")
 	public List<Cake> getCakelist();
 
 }
